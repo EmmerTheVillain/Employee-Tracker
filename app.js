ea079bc1ac1df027134f84f8e5e13583a03db2bd
Mike Barber <bandofgold1@gmail.com>
2023-10-02 13:46:52 -0400

    added view queries

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const inquirer = require('inquirer');
-const pool = require('./assets/config/connections');
-const consoleTable = require('console.table');
 const { viewDepartments, viewRoles, viewEmployees, newDepartment, newRole, addEmployee, updateRole } = require('./utils/queries');
 
 function init() {
@@ -20,40 +18,40 @@ function init() {
           'Quit'
         ]
       })
-      .then((answer) => {
-        switch (answer.action) {
-        case 'Create a new Department':
-            newDepartment(init);
-            break;
-        case 'Create a new Role':
-            newRole(init);
-            break;
-        case 'Add a new Employee':
-            addEmployee(init);
-            break;          
-        case 'Update an existing Employee Role':
-            updateRole(init);
-            break;
-        case 'View Departments':
-            viewDepartments(init);
-            break;
-        case 'View Roles':
-            viewRoles(init);
-            break;
-        case 'View Employees':
-            viewEmployees(init);     
-            break;     
-        case 'Quit':
-            pool.end();
-            console.log('good-bye');
-            break;
-        default:
-            console.log('Invalid choice. Please select again.');
-            init();
-        }
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
+    //   .then((answer) => {
+    //     switch (answer.action) {
+    //     case 'Create a new Department':
+    //         newDepartment(init);
+    //         break;
+    //     case 'Create a new Role':
+    //         newRole(init);
+    //         break;
+    //     case 'Add a new Employee':
+    //         addEmployee(init);
+    //         break;          
+    //     case 'Update an existing Employee Role':
+    //         updateRole(init);
+    //         break;
+    //     case 'View Departments':
+    //         viewDepartments(init);
+    //         break;
+    //     case 'View Roles':
+    //         viewRoles(init);
+    //         break;
+    //     case 'View Employees':
+    //         viewEmployees(init);     
+    //         break;     
+    //     case 'Quit':
+    //         pool.end();
+    //         console.log('good-bye');
+    //         break;
+    //     default:
+    //         console.log('Invalid choice. Please select again.');
+    //         init();
+    //     }
+    //   })
+    //   .catch((error) => {
+    //     console.error('Error:', error);
+    //   });
   }
 init();
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 require('dotenv').config();
 const inquirer = require('inquirer');
-
+const init = require('./app.js')
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -12,3 +12,5 @@ app.use(express.json());
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
+
+  init();
