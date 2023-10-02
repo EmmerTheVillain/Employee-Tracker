13a1cf56f36e545c69d3edb3f3e81c5c590e5c9e
Mike Barber <bandofgold1@gmail.com>
2023-10-02 13:15:33 -0400

    updated server.js

--- server.js
+++ server.js
@@ -5,3 +5,10 @@ const inquirer = require('inquirer');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
